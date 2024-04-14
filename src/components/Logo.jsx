5202d907d2eeb0e8e02352d2a734cbbfa7383284
Jaguruth-N <jagruthjain118@gmail.com>
2024-04-14 15:29:06 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Logo({ width = "100%" }) {
+  return (
+    // <img src="react-english\11blogapp\src\assets\unicorn.png" style={{ width: width }} alt='logo' />
+    <h1 className='text-xl font-semibold'>MegaBlog</h1>
+  )
+}
+
+export default Logo
