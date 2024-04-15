6325705a62120d6775b26e4c3ff90d37154eaffb
Jaguruth-N <jagruthjain118@gmail.com>
2024-04-15 23:46:50 +0530

    commit 3

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux"
 import authService from "../../appwrite/auth.js"
-import {logout } from "../../store/authSlice.js"
+import { logout } from "../../store/authSlice.js"
+import { useNavigate } from 'react-router-dom'
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
-
-    const lougoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout());
-        })
-    }
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const lougoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+      navigate('/home')
+    })
+  }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={lougoutHandler}
+      className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+      onClick={lougoutHandler}
     >Logout</button>
   )
 }
